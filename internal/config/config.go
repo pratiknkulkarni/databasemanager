bc137e557040016a461f06cbdfa41137729c4f80
pratik kulkarni <pnkulkarni@pm.me>
2025-10-20 05:25:55 +0000

    add initial docs for config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,7 @@
 package config
 
-import "fmt"
-
+// DatabaseConfig holds the configuration for the database. Includes PostgreSQL and MySQL.
+// Keeping the same format as the databases increase.
 type DatabaseConfig struct {
 	DatabaseHostname string `mapstructure:"database_hostname"`
 	DatabasePort     int    `mapstructure:"database_port"`
@@ -10,6 +10,8 @@ type DatabaseConfig struct {
 	DatabaseName     string `mapstructure:"database_name"`
 }
 
+// InfisicalConfig holds the configuration details for Infisical.
+// Deprecated: I am directly putting these in the Config struct
 type InfisicalConfig struct {
 	InfisicalProjectId    string `mapstructure:"infisical_project_id"`
 	InfisicalClientId     string `mapstructure:"infisical_client_id"`
@@ -17,34 +19,13 @@ type InfisicalConfig struct {
 	InfisicalSiteURL      string `mapstructure:"infisical_site_url"`
 }
 
+// Config holds the entire configuration including database and Infisical. including database and Infisica
 type Config struct {
-	Postgres              DatabaseConfig `mapstructure:"postgres"`
-	MySQL                 DatabaseConfig `mapstructure:"mysql"`
-	InfisicalProjectId    string         `mapstructure:"infisical_project_id"`
-	InfisicalClientId     string         `mapstructure:"infisical_client_id"`
-	InfisicalClientSecret string         `mapstructure:"infisical_client_secret"`
-	InfisicalSiteURL      string         `mapstructure:"infisical_site_url"`
+	Postgres DatabaseConfig `mapstructure:"postgres"`
+	MySQL    DatabaseConfig `mapstructure:"mysql"`
+
+	InfisicalProjectId    string `mapstructure:"infisical_project_id"`
+	InfisicalClientId     string `mapstructure:"infisical_client_id"`
+	InfisicalClientSecret string `mapstructure:"infisical_client_secret"`
+	InfisicalSiteURL      string `mapstructure:"infisical_site_url"`
 }
-
-func (c *Config) GetDatabaseConfig(dbType string) (*DatabaseConfig, error) {
-	switch dbType {
-	case "postgres":
-		if c.Postgres.DatabaseHostname == "" {
-			return nil, fmt.Errorf("postgres configuration not found")
-		}
-		return &c.Postgres, nil
-
-	case "mysql":
-		if c.MySQL.DatabaseHostname == "" {
-			return nil, fmt.Errorf("mysql configuration not found")
-		}
-		return &c.MySQL, nil
-
-	default:
-		return nil, fmt.Errorf("unsupported database type: %s", dbType)
-	}
-}
-
-//func NewConfig(infisicalConfig InfisicalConfig, databaseConfig DatabaseConfig) *Config {
-//	return &Config{DatabaseConfig: databaseConfig, InfisicalConfig: infisicalConfig}
-//}
