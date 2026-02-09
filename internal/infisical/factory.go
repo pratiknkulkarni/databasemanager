38865cd108b6e459b43faf95a58ce0287a0f6b0c
Pratik Kulkarni <pnkulkarni@proton.me>
2026-02-09 18:14:24 +0530

    refactor: implement dependency injection container and strict config
    - update main entry point to initialize application state.
    - decouple business logic from CLI commands
    - implemented a factory method for Infisical initialiation.

---
+++ internal/infisical/factory.go
@@ -0,0 +1,26 @@
+package infisical
+
+import (
+	"context"
+	"fmt"
+
+	infisical "github.com/infisical/go-sdk"
+	"github.com/praaatik/databasemanager/internal/config"
+)
+
+func NewClient(cfg *config.Config) (infisical.InfisicalClientInterface, error) {
+	client := infisical.NewInfisicalClient(context.Background(), infisical.Config{
+		SiteUrl:              cfg.InfisicalSiteURL,
+		AutoTokenRefresh:     true,
+		CacheExpiryInSeconds: 0,
+	})
+
+	//fmt.Println(client)
+
+	_, err := client.Auth().UniversalAuthLogin(cfg.InfisicalClientID, cfg.InfisicalClientSecret)
+	if err != nil {
+		return nil, fmt.Errorf("authentication failed: %w", err)
+	}
+
+	return client, nil
+}
